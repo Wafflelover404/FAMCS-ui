094fcd2a244a4d9167168374c7e0391b8fe30d03
Afanasieff Ivan <afanasieffivan@gmail.com>
2026-07-25 23:50:00 +0300

    feat(FAM-97): add app bootstrap

---
+++ app/frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react" 
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import "./index.css"
+import App from "./App.tsx"
+import { useUiStore } from "./store/useUiStore.ts"
+
+document.documentElement.setAttribute("data-theme", useUiStore.getState().theme)
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
