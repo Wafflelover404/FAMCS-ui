9d9de8ad9dc3b066ef0652b3966d6c4f3a8f72f9
Afanasieff Ivan <afanasieffivan@gmail.com>
2026-07-25 21:10:00 +0300

    feat(FAM-83): add params panel (final)

--- app/frontend/src/components/ParamsPanel.tsx
+++ app/frontend/src/components/ParamsPanel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react" 
+import { useEffect, useState } from "react"
 import { useBoardStore } from "../store/useBoardStore"
 import { useSolveStore } from "../store/useSolveStore"
 import * as api from "../lib/api"
